32222ae03b6e90bde5a6c0768b32f3c64d52d377
abhi4gitt <raiabhishek631@gmail.com>
2026-01-01 16:43:29 +0530

    Initial Commit

---
+++ JurrasicParkPredators/JurrasicParkPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JurrasicParkPredatorsApp.swift
+//  JurrasicParkPredators
+//
+//  Created by Abhishek on 01/01/26.
+//
+
+import SwiftUI
+
+@main
+struct JurrasicParkPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
